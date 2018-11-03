547ceac993c571374d404850d7c76eee582d31f9
technosoul <rockdrigo.mtz@gmail.com>
2018-11-02 18:55:39 -0600

    constant example

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
 }
